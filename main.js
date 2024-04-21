988df7acb4cfa52e42fc4ed41bb56b0d9d87729d
Haris Sabanovic <hasa2303@student.miun.se>
2024-04-21 16:10:42 +0200

    funktion som visar jobb på hemsida

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ async function getJobs(){
    try {
      const respone = await fetch("/api/workers");
      const data = await respone.json();
+
+     displayData(data)
    } catch (error) {
      console.error("Error occured fetching data: " + error)
    }
@@ -12,5 +14,40 @@ async function getJobs(){
 
 
 function displayData(data) {
-    const containerEl = document
+
+    const containerEl = document.getElementById("data-container");
+
+    //rensar innehållet innan 
+    containerEl.innerHTML = "";
+
+    data.forEach(job => {
+        const jobDivEl = document.createElement("div");
+        jobDivEl.classList.add("job-container");
+
+
+        const companyNameEl = document.createElement("h3");
+        companyNameEl.innerHTML = `${job.companyname}`;
+
+        const jobTitleEl = document.createElement("p");
+        jobTitleEl.innerHTML = `${job.jobtitle}`;
+
+        const locationEl = document.createElement("p");
+        locationEl.innerHTML = `${job.location}`;
+
+        const dateEl = document.createElement("p");
+        dateEl.innerHTML = `${job.startdate} - ${job.enddate}`;
+
+        const infoEl = document.createElement("p");
+        infoEl.innerHTML = `${job.description}`;
+
+
+        jobDivEl.appendChild(companyNameEl);
+        jobDivEl.appendChild(jobTitleEl);
+        jobDivEl.appendChild(locationEl);
+        jobDivEl.appendChild(dateEl);
+        jobDivEl.appendChild(infoEl);
+
+        containerEl.appendChild(jobDivEl);
+
+    })
 }
